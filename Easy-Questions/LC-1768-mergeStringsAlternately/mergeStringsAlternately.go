21ec32d8061ffabe26a2f860ae5473db1c7f5c99
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2024-03-03 20:54:32 +0530

    leetcode

---
+++ Easy-Questions/LC-1768-mergeStringsAlternately/mergeStringsAlternately.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	word1 := "ab"
+	word2 := "pqrs"
+	// Output: "apbqcr"
+	// Explanation: The merged string will be merged as so:
+	// word1:  a   b   c
+	// word2:    p   q   r
+	// merged: a p b q c r
+	fmt.Println("Resulut := ", mergeAlternately(word1, word2))
+}
+
+func mergeAlternately(word1 string, word2 string) string {
+	var newstring string
+	count := 1
+	for i := 0; i < len(word1); i++ {
+
+		newstring += string(word1[i])
+		newstring += string(word2[i])
+
+		if count == len(word1) {
+			newstring += string(word2[i+1:])
+			break
+		}
+		if count == len(word2) {
+			newstring += string(word1[i+1:])
+			break
+		}
+		count++
+	}
+
+	return newstring
+}
