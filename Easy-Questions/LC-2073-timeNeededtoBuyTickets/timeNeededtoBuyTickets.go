2e750dd9cb7cefb6f97a68dea99e3213b20de89a
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2024-04-12 10:30:00 +0530

    leetcode

--- Easy-Questions/LC-2073-timeNeededtoBuyTickets/timeNeededtoBuyTickets.go
+++ Easy-Questions/LC-2073-timeNeededtoBuyTickets/timeNeededtoBuyTickets.go
@@ -10,7 +10,6 @@ func main() {
 	// - In the first pass, everyone in the line buys a ticket and the line becomes [1, 2, 1].
 	// - In the second pass, everyone in the line buys a ticket and the line becomes [0, 1, 0].
 	// The person at position 2 has successfully bought 2 tickets and it took 3 + 3 = 6 seconds.
-	//  spush
 	fmt.Println("Result := ",timeRequiredToBuy(tickets,k))
 }
 
